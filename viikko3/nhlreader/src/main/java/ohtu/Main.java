a2c9a8c6dbe16090e144985e66037544a59c2905
tnli <tnli@iki.fi>
2020-11-16 20:50:47 +0200

    viikko 3, tehtävä 1

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -3,6 +3,10 @@ package ohtu;
 import com.google.gson.Gson;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,16 +14,23 @@ public class Main {
         
         String bodyText = Request.Get(url).execute().returnContent().asString();
                 
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
+        //System.out.println("json-muotoinen data:");
+        //System.out.println( bodyText );
+
+        DateFormat aikaleimamuoto = new SimpleDateFormat("E MMM HH:mm:ss zz y");
+        Date aikaleima = new Date();
 
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
         
-        System.out.println("Oliot:");
+        System.out.println("Players from FIN " + aikaleimamuoto.format(aikaleima));
         for (Player player : players) {
-            System.out.println(player);
+            if (player.getNationality().equals("FIN")) {
+                System.out.println(player);
+            }
         }   
     }
   
 }
+
+// Wed Nov 06 23:31:32 EET 2019
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,7 +3,19 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    // private int penalties;
+    private String team;
+    // private int games;
 
+    @Override
+    public String toString() {
+        return name + " team " + team + " goals " + goals + " assists " + assists;
+    }
+
+    // methods for name
     public void setName(String name) {
         this.name = name;
     }
@@ -12,9 +24,40 @@ public class Player {
         return name;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    // methods for nationality
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    // methods for assists
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+    
+    public int getAssists() {
+        return assists;
+    }
+    
+    // methods for goals
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+    
+    public int getGoals() {
+        return goals;
+    }
+
+    // methods for team
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    public String getTeam() {
+        return team;
     }
       
 }
